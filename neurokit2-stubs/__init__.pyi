40808c321de423148cfb1e11eacd1758071e5929
danibene <34680344+danibene@users.noreply.github.com>
2023-05-30 13:44:37 -0400

    remove module imports from init

--- neurokit2-stubs/__init__.pyi
+++ neurokit2-stubs/__init__.pyi
@@ -1,12 +1,4 @@
 """Top-level package for neurokit2-stubs."""
-from neurokit2 import (
-    ecg as ecg,
-    eda as eda,
-    hrv as hrv,
-    rsp as rsp,
-    signal as signal,
-)
-
 from .ecg import *
 from .eda import *
 from .hrv import *
