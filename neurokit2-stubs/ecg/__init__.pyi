5f8ff6254a90cbd887aa907cd0fbc7ed21db3f7f
danibene <34680344+danibene@users.noreply.github.com>
2023-05-30 12:14:37 -0400

    add ecg_simulate

--- neurokit2-stubs/ecg/__init__.pyi
+++ neurokit2-stubs/ecg/__init__.pyi
@@ -1,9 +1,11 @@
 from neurokit2.ecg.ecg_process import ecg_process as ecg_process
 from neurokit2.ecg.ecg_clean import ecg_clean as ecg_clean
 from neurokit2.ecg.ecg_peaks import ecg_peaks as ecg_peaks
+from neurokit2.ecg.ecg_simulate import ecg_simulate as ecg_simulate
 
 __all__ = [
     "ecg_process",
     "ecg_clean",
     "ecg_peaks",
+    "ecg_simulate",
 ]
---
+++ neurokit2-stubs/ecg/ecg_simulate.pyi
@@ -0,0 +1,18 @@
+from typing import Optional, Union
+
+import numpy as np
+import pandas as pd
+
+def ecg_simulate(
+    duration: int = 10,
+    length: Optional[int] = None,
+    sampling_rate: int = 1000,
+    noise: float = 0.01,
+    heart_rate: int = 70,
+    heart_rate_std: int = 1,
+    method: str = "ecgsyn",
+    random_state: Union[None, int, np.random.RandomState, np.random.Generator] = None,
+    random_state_distort: Union[str, None, int, np.random.RandomState, np.random.Generator] = "spawn",
+    **kwargs,
+) -> Union[np.ndarray, pd.DataFrame]:
+    ...
