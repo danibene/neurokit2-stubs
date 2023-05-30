bca0c1546322f71148fe5ec54daa09564632aa37
danibene <34680344+danibene@users.noreply.github.com>
2023-05-30 11:32:58 -0400

    update ecg init

--- neurokit2-stubs/ecg/__init__.pyi
+++ neurokit2-stubs/ecg/__init__.pyi
@@ -1,7 +1,9 @@
 from neurokit2.ecg.ecg_process import ecg_process as ecg_process
 from neurokit2.ecg.ecg_clean import ecg_clean as ecg_clean
+from neurokit2.ecg.ecg_peaks import ecg_peaks as ecg_peaks
 
 __all__ = [
     "ecg_process",
     "ecg_clean",
+    "ecg_peaks",
 ]
