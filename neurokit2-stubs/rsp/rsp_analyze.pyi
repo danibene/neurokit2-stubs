9ba5407817275f431662ef1c38ab928ed93f7659
danibene <34680344+danibene@users.noreply.github.com>
2023-05-30 10:31:04 -0400

    remove unnecessary import

--- neurokit2-stubs/rsp/rsp_analyze.pyi
+++ neurokit2-stubs/rsp/rsp_analyze.pyi
@@ -1,7 +1,6 @@
 import pandas as pd
 from typing import Union
-from pandas import DataFrame
 
-def rsp_analyze(data: Union[dict, DataFrame], sampling_rate: int = 1000, method: str = "auto") -> DataFrame:
+def rsp_analyze(data: Union[dict, pd.DataFrame], sampling_rate: int = 1000, method: str = "auto") -> pd.DataFrame:
     ...
 
