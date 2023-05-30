a4d0676b0c31e3e4a9f069c2188294d509e66a3c
danibene <34680344+danibene@users.noreply.github.com>
2023-05-30 11:40:11 -0400

    add eda to main init

--- neurokit2-stubs/__init__.pyi
+++ neurokit2-stubs/__init__.pyi
@@ -1,18 +1,21 @@
 """Top-level package for neurokit2-stubs."""
 from neurokit2 import (
     ecg as ecg,
+    eda as eda,
     hrv as hrv,
     rsp as rsp,
     signal as signal,
 )
 
 from .ecg import *
+from .eda import *
 from .hrv import *
 from .rsp import *
 from .signal import *
 
 __all__ = [
     "ecg",
+    "eda",
     "hrv",
     "rsp",
     "signal",
