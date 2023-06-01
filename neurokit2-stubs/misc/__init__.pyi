526a4a7a416c4a6f8cd4b939342c7953f2280b1f
danibene <34680344+danibene@users.noreply.github.com>
2023-06-01 15:29:35 -0400

    Create __init__.pyi

---
+++ neurokit2-stubs/misc/__init__.pyi
@@ -0,0 +1,9 @@
+from neurokit2.misc.random import check_random_state as check_random_state
+from neurokit2.misc.random import spawn_random_state as spawn_random_state
+from neurokit2.misc.random import check_random_state_children as check_random_state_children
+
+__all__ = [
+    "check_random_state",
+    "spawn_random_state",
+    "check_random_state_children",
+]
