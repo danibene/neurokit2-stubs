5385958d1f9e2f99f65498d56a01e1dc28438f9e
danibene <34680344+danibene@users.noreply.github.com>
2023-08-17 18:13:24 -0400

    update __init__.pyi with signal_distort

--- neurokit2-stubs/signal/__init__.pyi
+++ neurokit2-stubs/signal/__init__.pyi
@@ -1,9 +1,11 @@
 from neurokit2.signal.signal_resample import signal_resample as signal_resample
 from neurokit2.signal.signal_power import signal_power as signal_power
 from neurokit2.signal.signal_simulate import signal_simulate as signal_simulate
+from neurokit2.signal.signal_distort import signal_distort as signal_distort
 
 __all__ = [
     "signal_resample",
     "signal_power",
     "signal_simulate",
+    "signal_distort",
 ]
