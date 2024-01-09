0d57eb419f257c122f9adc238362bb1de4acc025
danibene <34680344+danibene@users.noreply.github.com>
2024-01-09 10:10:37 -0500

    Update __init__.pyi

--- neurokit2-stubs/signal/__init__.pyi
+++ neurokit2-stubs/signal/__init__.pyi
@@ -2,10 +2,12 @@ from neurokit2.signal.signal_resample import signal_resample as signal_resample
 from neurokit2.signal.signal_power import signal_power as signal_power
 from neurokit2.signal.signal_simulate import signal_simulate as signal_simulate
 from neurokit2.signal.signal_distort import signal_distort as signal_distort
+from neurokit2.signal.signal_interpolate import signal_interpolate as signal_interpolate
 
 __all__ = [
     "signal_resample",
     "signal_power",
     "signal_simulate",
     "signal_distort",
+    "signal_interpolate",
 ]
